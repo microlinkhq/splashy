86dd55c17c46131aa62e4342d345ebb0a43534d4
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-10-14 20:50:54 +0200

    Use lqip dep

--- index.js
+++ index.js
@@ -3,57 +3,36 @@
 const createTempFile = require('create-temp-file2')
 const universalify = require('universalify')
 const waterfall = require('run-waterfall')
-const dominant = require('huey/dominant')
-const image = require('get-image-data')
-const palette = require('huey/palette')
 const get = require('simple-get')
-const rgbHex = require('rgb-hex')
+const { palette } = require('lqip')
 const pump = require('pump')
 
-const rgbToHex = (...rgb) => `#${rgbHex(...rgb)}`.toUpperCase()
+const toPalette = universalify.fromPromise(palette)
+const createTypeError = prop =>
+  new TypeError(`Need to provide a valid ${prop}.`)
 
-const getDominantImageColor = data => {
-  const dominantColor = dominant(data)
-  return dominantColor ? rgbToHex(...dominantColor) : null
-}
+module.exports = (opts = {}) => {
+  const fromFile = (filepath, cb) => {
+    if (!filepath) return cb(createTypeError('file path'))
+    return toPalette(filepath, cb)
+  }
 
-const getPaletteImageColors = (data, opts) => {
-  const paletteColors = palette(data, opts)
-  return paletteColors ? paletteColors.map(rgb => rgbToHex(...rgb)) : null
-}
+  const fromUrl = (url, cb) => {
+    if (!url) return cb(createTypeError('image url'))
+    const tempFile = createTempFile(opts)
 
-const fromFile = (filepath, opts, cb) => {
-  if (!filepath) return cb(new TypeError('Need to provide a valid file path.'))
-  const { paletteColors = 3 } = opts
-
-  return waterfall(
-    [
-      next => image(filepath, next),
-      (img, next) =>
-        next(null, {
-          dominantColor: getDominantImageColor(img.data),
-          paletteColors: getPaletteImageColors(img.data, paletteColors)
-        })
-    ],
-    cb
-  )
-}
-
-const fromUrl = (url, opts = {}, cb) => {
-  if (!url) return cb(new TypeError('Need to provide a valid file URL.'))
-
-  const tempFile = createTempFile(opts.tempFileOpts)
-  return waterfall(
-    [
+    const tasks = [
       next => get(url, next),
       (res, next) => pump(res, tempFile, next),
-      next => fromFile(tempFile.path, opts, next),
-      (predominantColors, next) =>
-        tempFile.cleanup(() => next(null, predominantColors))
-    ],
-    cb
-  )
-}
+      next => fromFile(tempFile.path, next),
+      (paletteColors, next) => tempFile.cleanup(() => next(null, paletteColors))
+    ]
 
-module.exports = universalify.fromCallback(fromFile)
-module.exports.fromUrl = universalify.fromCallback(fromUrl)
+    return waterfall(tasks, cb)
+  }
+
+  return {
+    fromFile: universalify.fromCallback(fromFile),
+    fromUrl: universalify.fromCallback(fromUrl)
+  }
+}
