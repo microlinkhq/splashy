e02ac57edf047271958c1d7fcc42fca8e4a7abfb
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-12-11 11:21:05 +0100

    Remove callback

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const waterfall = require('run-waterfall')
+const temperment = require('temperment')
 const vibrant = require('node-vibrant')
 const { promisify } = require('util')
-const get = require('simple-get')
-const tempy = require('tempy')
-const pump = require('pump')
+const got = require('got')
 const fs = require('fs')
+
+const pump = promisify(require('pump'))
 
 const toPalette = swatch =>
   Object.keys(swatch)
@@ -19,38 +19,27 @@ const toPalette = swatch =>
     .sort((a, b) => a.popularity <= b.popularity)
     .map(color => color.hex)
 
-const getPalette = (filepath, cb) =>
-  vibrant
-    .from(filepath)
-    .getPalette((err, swatch) => (err ? cb(err) : cb(null, toPalette(swatch))))
+const getPalette = async filepath => {
+  const swatch = await vibrant.from(filepath).getPalette()
+  return toPalette(swatch)
+}
 
 const createTypeError = prop =>
   new TypeError(`Need to provide a valid ${prop}.`)
 
-module.exports = (opts = {}) => {
-  const fromFile = (filepath, cb) => {
-    if (!filepath) return cb(createTypeError('file path'))
-    return getPalette(filepath, cb)
-  }
-
-  const fromUrl = (url, cb) => {
-    if (!url) return cb(createTypeError('image url'))
-    const tmpPath = tempy.file(opts)
-    const writeStream = fs.createWriteStream(tmpPath)
-
-    const tasks = [
-      next => get(url, next),
-      (res, next) => pump(res, writeStream, next),
-      next => fromFile(tmpPath, next),
-      (paletteColors, next) =>
-        fs.unlink(tmpPath, err => next(err, paletteColors))
-    ]
-
-    return waterfall(tasks, cb)
-  }
-
-  return {
-    fromFile: promisify(fromFile),
-    fromUrl: promisify(fromUrl)
-  }
+const fromUrl = async (url, opts) => {
+  if (!url) throw createTypeError('image url')
+  const temp = temperment.standalone()
+  const tmpPath = temp.file(opts)
+  await pump(got.stream(url, opts), fs.createWriteStream(tmpPath))
+  const paletteColors = await fromFile(tmpPath)
+  await temp.cleanup()
+  return paletteColors
 }
+
+const fromFile = async filepath => {
+  if (!filepath) throw createTypeError('file path')
+  return getPalette(filepath)
+}
+
+module.exports = { file: fromFile, url: fromUrl }
--- test/index.js
+++ test/index.js
@@ -1,68 +1,15 @@
 'use strict'
 
-const should = require('should')
+const test = require('ava')
 const path = require('path')
+const splashy = require('..')
 
-const splashy = require('..')()
+test('from url', async t => {
+  const colors = await splashy.url('https://i.imgur.com/ZJDyOhn.jpg')
+  t.snapshot(colors)
+})
 
-const filepath = path.resolve(__dirname, 'jerry.jpg')
-const fileUrl = 'https://i.imgur.com/ZJDyOhn.jpg'
-
-describe('get predominant colors', () => {
-  describe('callback', () => {
-    it('from a file', done => {
-      splashy.fromFile(filepath, (err, paletteColors) => {
-        should(paletteColors).be.eql([
-          '#941c1c',
-          '#841c16',
-          '#ad685e',
-          '#d48c74',
-          '#6c5444',
-          '#cca4a4'
-        ])
-        done(err)
-      })
-    })
-
-    it('from an url', done => {
-      splashy.fromUrl(fileUrl, (err, paletteColors) => {
-        should(paletteColors).be.eql([
-          '#941c1c',
-          '#841c16',
-          '#ad685e',
-          '#d48c74',
-          '#6c5444',
-          '#cca4a4'
-        ])
-        done(err)
-      })
-    })
-  })
-
-  describe('promise', () => {
-    it('from a file', async () => {
-      const paletteColors = await splashy.fromFile(filepath)
-      should(paletteColors).be.eql([
-        '#941c1c',
-        '#841c16',
-        '#ad685e',
-        '#d48c74',
-        '#6c5444',
-        '#cca4a4'
-      ])
-    })
-
-    it('from an url', async () => {
-      const paletteColors = await splashy.fromUrl(fileUrl)
-
-      should(paletteColors).be.eql([
-        '#941c1c',
-        '#841c16',
-        '#ad685e',
-        '#d48c74',
-        '#6c5444',
-        '#cca4a4'
-      ])
-    })
-  })
+test('from file', async t => {
+  const colors = await splashy.file(path.resolve(__dirname, 'jerry.jpg'))
+  t.snapshot(colors)
 })
