18bae7dff109628ae2012d165d6e7fca0ba4da06
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-12-11 11:45:29 +0100

    Sort values

--- test/index.js
+++ test/index.js
@@ -1,15 +1,31 @@
 'use strict'
 
+const { isEmpty, clone, pull } = require('lodash')
+const hexSorter = require('hexsorter')
 const test = require('ava')
 const path = require('path')
+
 const splashy = require('..')
+
+const sortColors = colors => {
+  const input = clone(colors)
+  const output = []
+
+  while (!isEmpty(input)) {
+    const color = hexSorter.mostBrightColor(input)
+    pull(input, color)
+    output.push(color)
+  }
+
+  return output
+}
 
 test('from url', async t => {
   const colors = await splashy.url('https://i.imgur.com/ZJDyOhn.jpg')
-  t.snapshot(colors)
+  t.snapshot(sortColors(colors))
 })
 
 test('from file', async t => {
   const colors = await splashy.file(path.resolve(__dirname, 'jerry.jpg'))
-  t.snapshot(colors)
+  t.snapshot(sortColors(colors))
 })
