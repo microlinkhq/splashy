e2fc257d7cc554434b88e5e63aadbab3fada41c8
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-06-03 19:50:39 +0200

    feat: consistent results

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const toPalette = swatch =>
   Object.keys(swatch)
     .reduce((acc, key) => {
       const value = swatch[key]
-      if (!value) return acc
       acc.push({ popularity: value.getPopulation(), hex: value.getHex() })
       return acc
     }, [])
     .sort((a, b) => a.popularity <= b.popularity)
     .map(color => color.hex)
 
-module.exports = async input =>
-  toPalette(await vibrant.from(input).getPalette())
+module.exports = async input => {
+  let swatch
+
+  try {
+    swatch = await vibrant.from(input).getPalette()
+  } catch (err) {
+    swatch = {}
+  }
+
+  return toPalette(swatch)
+}
--- test/index.js
+++ test/index.js
@@ -1,12 +1,15 @@
 'use strict'
 
 const { isEmpty, clone, pull } = require('lodash')
+
 const hexSorter = require('hexsorter')
 const path = require('path')
 const test = require('ava')
 const fs = require('fs')
 
 const splashy = require('..')
+const FIXTURES_PATH = path.resolve(__dirname, 'fixtures')
+const images = fs.readdirSync(FIXTURES_PATH)
 
 const sortColors = colors => {
   const input = clone(colors)
@@ -21,9 +24,12 @@ const sortColors = colors => {
   return output
 }
 
-test('get predominant colors', async t => {
-  const filepath = path.resolve(__dirname, 'jerry.jpg')
-  const buffer = fs.readFileSync(filepath)
-  const colors = await splashy(buffer)
-  t.snapshot(sortColors(colors))
+images.forEach(image => {
+  const extension = path.extname(image)
+  test(extension, async t => {
+    const filepath = path.resolve(path.resolve(FIXTURES_PATH, image))
+    const buffer = fs.readFileSync(filepath)
+    const colors = await splashy(buffer)
+    t.snapshot(sortColors(colors))
+  })
 })
